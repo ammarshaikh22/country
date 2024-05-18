51b8f349e85dd017030e34d4d5a74629dec5b860
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-05-18 22:42:17 +0500

    ee

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-export const Box = ({ name, src, capital, }) => {    
+export const Box = ({ name, src, capital, }) => {
     return (
         <div className="box">
-            <Link to={`/country/${name}`} >
-                <img src={src} alt="flag" />
-            </Link>
+            <div className="img">
+                <Link to={`/country/${name}`} >
+                    <img src={src} alt="flag" />
+                </Link>
+            </div>
             <div className="text">
                 <h1>{name}</h1>
                 <h4>{capital}</h4>
