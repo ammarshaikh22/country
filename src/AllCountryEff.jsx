462b3b4111b69d8a96c9a6037036cd98c6ac3a4e
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-05-18 23:21:55 +0500

    www

--- src/AllCountryEff.jsx
+++ src/AllCountryEff.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 export const AllCountryEff = () => {
   return Array.from({ length: 50 }).map(() => {
-    return <div className="box effectBox"></div>;
+    return <div className="box"></div>;
   });
 };
